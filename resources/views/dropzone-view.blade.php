1988ba4a467c7b7a99b81eae55a9b7208da91aea
Bike Del <home@Homes-iMac.Home>
2017-03-23 18:05:11 +0200

    drop css

--- resources/views/dropzone-view.blade.php
+++ resources/views/dropzone-view.blade.php
@@ -9,7 +9,14 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css" rel="stylesheet">
      <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/min/dropzone.min.js"></script>
 
-
+    <style>
+        .dropzone {
+            height: 200px;
+            width: 500px;
+            border: dashed 1px red;
+            background-color: lightblue;
+        }
+    </style>
 @section('content')
 
 <div class="container">

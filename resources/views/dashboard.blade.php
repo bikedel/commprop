4ed8155963784eb7e6dbfe9f6fa910c267abf4fc
Bike Del <home@Homes-iMac.Home>
2017-03-23 19:34:31 +0200

    dash
    temporary put status -1
    
    pointing to array id and not actual id

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -156,7 +156,7 @@ mySidenav {
 
         <span class="w3-small">Erf: {{$properties[$x]->erf }} <br>Unit: {{$properties[$x]->id}} <br>
            @if ( sizeof($properties[$x]->units) > 0 )
-                  Status: {{ $statuses[$properties[$x]->units[0]->status_id]->name}}</span><br>
+                  Status: {{ $statuses[$properties[$x]->units[0]->status_id-1]->name}}</span><br>
            @endif
 
       </li>

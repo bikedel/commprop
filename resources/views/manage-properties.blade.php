bddf47883833220fec2e1a61dc6ebeabfd4763b6
Bike Del <home@Homes-iMac.Home>
2017-04-01 22:39:30 +0200

    ===

--- resources/views/manage-properties.blade.php
+++ resources/views/manage-properties.blade.php
@@ -897,8 +897,8 @@ line-height: 1.8;
 
                         <table class="table  table-hover"  >
 
-                            <tr v-for="item  in orderBy(fillNote.note, 'date', -1)   " v-if=" item.unit_id == fillNote.unit_id ">
-                          <!--  == for online === for local <tr v-for="item  in fillNote.note | orderBy 'unit_id' -1 | orderBy 'date' -1" v-if=" item.unit_id === fillNote.unit_id "> -->
+                            <tr v-for="item  in orderBy(fillNote.note, 'date', -1)   " v-if=" item.unit_id === fillNote.unit_id ">
+                          <!--  === for online == for local <tr v-for="item  in fillNote.note | orderBy 'unit_id' -1 | orderBy 'date' -1" v-if=" item.unit_id === fillNote.unit_id "> -->
                                 <td style="white-space:pre-wrap ; word-wrap:break-word;">Unit @{{ item.unit_id  }} <blue> @{{  item.date |  dateFrom }}  <red>@{{ users[ item.user_id -1 ].name  }}</red></blue><br>@{{ item.description }}</td>
 
                             </tr>
@@ -984,8 +984,8 @@ line-height: 1.8;
                                 <th width="180px">Date</th>
 
                             </tr>
-                           <!--    == for online === for local  <tr v-for="item  in fillOwner.owners | orderBy 'unit_id' -1 | orderBy 'date' -1" v-if=" item.unit_id === fillOwner.unit_id "> -->
-                            <tr v-for="item  in orderBy(fillOwner.owners, 'created_at', -1) " v-if=" item.unit_id == fillOwner.unit_id ">
+                           <!--    === for online == for local  <tr v-for="item  in fillOwner.owners | orderBy 'unit_id' -1 | orderBy 'date' -1" v-if=" item.unit_id === fillOwner.unit_id "> -->
+                            <tr v-for="item  in orderBy(fillOwner.owners, 'created_at', -1) " v-if=" item.unit_id === fillOwner.unit_id ">
 
 
                                  <td>@{{ contacttypes[item.contact_type_id-1].name }}</td>

801a8a61a7eef4157eb2245ec9ec6a5ba187fe60
Bike Del <home@Homes-iMac.Home>
2017-04-01 23:21:12 +0200

    dash

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -131,7 +131,31 @@ mySidenav {
           <h3>{{$units->where('status_id','=','2')->count()}}</h3>
         </div>
         <div class="w3-clear"></div>
-        <h4>FOwner Occupied</h4>
+        <h4>Owner Occupied</h4>
+      </div>
+    </div>
+  </div>
+
+    <div class="w3-quarter">
+      <div class="w3-container w3-khaki w3-text-white w3-padding-16">
+        <div class="w3-left"><i class="fa fa-credit-card w3-xxxlarge"></i></div>
+        <div class="w3-right">
+          <h3>{{$units->where('status_id','=','3')->count()}}</h3>
+        </div>
+        <div class="w3-clear"></div>
+        <h4>Tenant Occupied</h4>
+      </div>
+    </div>
+  </div>
+
+      <div class="w3-quarter">
+      <div class="w3-container w3-lightgray w3-text-white w3-padding-16">
+        <div class="w3-left"><i class="fa fa-credit-card w3-xxxlarge"></i></div>
+        <div class="w3-right">
+          <h3>{{$units->where('status_id','=','4')->count()}}</h3>
+        </div>
+        <div class="w3-clear"></div>
+        <h4>Govt/Provincial</h4>
       </div>
     </div>
   </div>

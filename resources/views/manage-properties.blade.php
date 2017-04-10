2de7bacf7b62fd1285fdff3a6d00e4151b6cf8f2
Bike Del <home@Homes-iMac.Home>
2017-04-10 23:24:42 +0200

    unit table

--- resources/views/manage-properties.blade.php
+++ resources/views/manage-properties.blade.php
@@ -492,10 +492,10 @@ line-height: 1.8;
                     <thead>
                          <tr>
                             <th width="120px" class="hidden-xs">Unit ID</th>
-                            <th width="180px">Status</th>
-                            <th width="180px">Type</th>
+                            <th width="120px">Status</th>
+                            <th width="240px">Type</th>
                             <th width="100px">Size</th>
-                            <th width="120px">Price</th>
+                            <th width="140px">Price</th>
                             <th width="220px">Actions</th>
 
                          </tr>

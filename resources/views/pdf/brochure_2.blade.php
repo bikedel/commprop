5501e4bcb27ca654ec596bf865c8cb5aad624a31
Bike Del <home@Homes-iMac.Home>
2017-05-26 08:54:12 +0200

    broth

--- resources/views/pdf/brochure_2.blade.php
+++ resources/views/pdf/brochure_2.blade.php
@@ -219,9 +219,9 @@ padding:0px;
 @foreach( $locations as $loc)
 
     @if (file_exists(public_path('/img/marker'.($loop->index+1).'.png')))
-     <p class="w3-text-blue pmap">  <img src="http://www.sircommdb.co.za/marker32_{{$loop->index+1}}.png"   > {{$loc}}</p>
+     <p class=" pmap">  <img src="http://www.sircommdb.co.za/marker32_{{$loop->index+1}}.png"   > {{$loc}}</p>
     @else
-     <p class="w3-text-blue pmap"> <img src="http://www.sircommdb.co.za/marker32.png"   >  {{$loc}}</p>
+     <p class=" pmap"> <img src="http://www.sircommdb.co.za/marker32.png"   >  {{$loc}}</p>
     @endif
 
 
@@ -236,7 +236,7 @@ padding:0px;
 <div class="container-fluid page-break">
 
 
-   <header><h4 class="w3-text-blue"><img src="http://www.sircommdb.co.za/marker32_{{$loop->index+1}}.png"  > {{$item->address }} </h4></header>
+   <header><h4 class=""><img src="http://www.sircommdb.co.za/marker32_{{$loop->index+1}}.png"  > {{$item->address }} </h4></header>
    <!-- <h4>{{$suburbs[$item->area_id]->name}}</h4> -->
 <img align="center" src = "http://maps.googleapis.com/maps/api/staticmap?size=600x200&zoom=16&markers=icon:http://www.sircommdb.co.za/marker32_{{$loop->index+1}}.png%7C{{$item->long}},{{$item->lat}}&maptype=hybrid&scale=20&key=AIzaSyCNgTdT8SN3jIzbdvZu7CBPKw3zz8J4Pww"  width="740" height="200" class='prop_img 'alt='Google Map'/>
 

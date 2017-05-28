0ee854bceaffc5e9057d4cc399a29029dee20bff
Bike Del <home@Homes-iMac.Home>
2017-05-28 15:48:42 +0200

    broth

--- resources/views/pdf/brochure.blade.php
+++ resources/views/pdf/brochure.blade.php
@@ -231,18 +231,6 @@ padding:0px;
 
 
 
-@foreach(array_chunk($address, 2) as $chunk)
-    <div class="row">
-        @foreach($chunk as $add)
-            <div class="col-md-6">
-                Some data
-            </div>
-        @endforeach
-    </div>
-@endforeach
-
-
-
 
 @foreach( $items as $item)
 

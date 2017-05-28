714652ac54a86f16701ae1925bc7ddc96f1cb6e5
Bike Del <home@Homes-iMac.Home>
2017-05-28 15:24:52 +0200

    broth

--- resources/views/pdf/brochure.blade.php
+++ resources/views/pdf/brochure.blade.php
@@ -235,7 +235,7 @@ padding:0px;
 <div class="container-fluid page-break">
 
 
-   <header><h4 class=""><img src="{{public_path('/img/marker'.($loop->index+1).'.png')}}"   > {{$item->address }} </h4></header>
+   <header><h4 class=""><img src="{{public_path('/img/marker'.($loop->index+1).'.png')}}"   width="30px" > {{$item->address }} </h4></header>
    <!-- <h4>{{$suburbs[$item->area_id]->name}}</h4> -->
 <img align="center" src = "http://maps.googleapis.com/maps/api/staticmap?size=740x200&zoom=16&markers=label:{{$loop->index+1}}%7C{{$item->long}},{{$item->lat}}&maptype=hybrid&scale=20&sensor=false&label=Hello&key=AIzaSyCNgTdT8SN3jIzbdvZu7CBPKw3zz8J4Pww"  width="740" height="200" class='prop_img 'alt='Google Map'/>
 

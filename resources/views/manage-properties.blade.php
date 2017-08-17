9dd415692b486c7b15a1e43fdcaad30a37fa4cfa
Bike Del <home@Homes-iMac.Home>
2017-08-17 18:20:51 +0200

    gps

--- resources/views/manage-properties.blade.php
+++ resources/views/manage-properties.blade.php
@@ -788,14 +788,14 @@ line-height: 1.8;
 
                     <div class="form-group">
                         <label for="Surname">Lat:</label>
-                        <input type="text" name="lat" class="form-control" v-model="fillItem.lat" />
-                        <span v-if="formErrorsUpdate['street']" class="error text-danger">@{{ formErrorsUpdate['lat'][0] }}</span>
+                        <input type="number" name="lat" class="form-control" v-model="fillItem.lat" />
+                        <span v-if="formErrorsUpdate['lat']" class="error text-danger">@{{ formErrorsUpdate['lat'][0] }}</span>
                     </div>
 
                     <div class="form-group">
                         <label for="Surname">Long:</label>
-                        <input type="text"  name="long" class="form-control" v-model="fillItem.long" />
-                        <span v-if="formErrorsUpdate['street']" class="error text-danger">@{{ formErrorsUpdate['long'][0] }}</span>
+                        <input type="number"  name="long" class="form-control" v-model="fillItem.long" />
+                        <span v-if="formErrorsUpdate['long']" class="error text-danger">@{{ formErrorsUpdate['long'][0] }}</span>
                     </div>
 
                     <div class="form-group">
